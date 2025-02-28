0940f4f52db9580927cfc1145a2231d890ffadd4
MeFisto94 <MeFisto94@users.noreply.github.com>
2025-02-28 23:24:59 +0100

    Improve handling of depth prepasses

--- rend3-routine/src/forward.rs
+++ rend3-routine/src/forward.rs
@@ -379,7 +379,7 @@ fn build_forward_pipeline_inner<M: Material>(
             strip_index_format: None,
             front_face: args.renderer.handedness.into(),
             cull_mode: Some(match args.routine_type {
-                RoutineType::Depth => wgpu::Face::Front,
+                RoutineType::Depth => wgpu::Face::Back,
                 RoutineType::Forward => wgpu::Face::Back,
             }),
             unclipped_depth: false,
@@ -388,7 +388,7 @@ fn build_forward_pipeline_inner<M: Material>(
         },
         depth_stencil: Some(DepthStencilState {
             format: TextureFormat::Depth32Float,
-            depth_write_enabled: true,
+            depth_write_enabled: matches!(args.routine_type, RoutineType::Depth),
             depth_compare: CompareFunction::GreaterEqual,
             stencil: StencilState::default(),
             bias: match args.routine_type {
